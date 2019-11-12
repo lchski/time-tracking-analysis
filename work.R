d991e008381502a41777006a34f36d8d9a83bf91
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-11-11 19:01:43 -0500

    werk

--- work.R
+++ work.R
@@ -1,28 +1,70 @@
-library(tidyverse)
-
 source("load.R")
 
 entries_work <- entries %>%
   filter(project == "Canadian Digital Service") %>%
-  select(date, note, hours)
+  select(date, note, hours) %>%
+  separate(note, c("group", "activity"), sep = ", ", extra = "merge") %>%
+  separate(group, c("group", "subgroup"), sep = "/", extra = "merge")
 
 entries_work %>%
-  separate(note, c("group", "activity"), sep = ", ", extra = "merge") %>%
-  separate(group, c("group", "subgroup"), sep = "/", extra = "merge") %>%
-  View()
-
-entries_work %>%
-  separate(note, c("group", "activity"), sep = ", ", extra = "merge") %>%
-  separate(group, c("group", "subgroup"), sep = "/", extra = "merge") %>%
   group_by(group, subgroup) %>%
   summarize(
     count = n(),
+    hours = sum(hours),
     first = min(date),
     last = max(date),
     time_between = time_length(interval(first, last), "days")
   ) %>%
   arrange(first, last) %>%
   View()
+
+entries_work %>%
+  mutate(
+    month = floor_date(date, unit = "month")
+  ) %>%
+  filter(
+    group %in% (entries_work %>%
+      group_by(group, subgroup) %>%
+      summarize(
+        count = n(),
+        hours = sum(hours),
+        first = min(date),
+        last = max(date),
+        time_between = time_length(interval(first, last), "days")
+      ) %>%
+      arrange(first, last) %>%
+      filter(count >= 10) %>%
+      select(group) %>%
+      unique() %>%
+      pull())
+  ) %>%
+  group_by(
+    group, month
+  ) %>%
+  summarize(hours = sum(hours)) %>%
+  ggplot(aes(x = month, y = hours, fill = group)) +
+  geom_col(position = "fill") +
+  scale_fill_brewer(palette = "RdGy")
+
+
+
+
+entries_work %>%
+  filter(group %in% (entries_work %>%
+    group_by(group, subgroup) %>%
+    summarize(
+      count = n(),
+      hours = sum(hours),
+      first = min(date),
+      last = max(date),
+      time_between = time_length(interval(first, last), "days")
+    ) %>%
+    arrange(first, last) %>%
+    filter(count < 10) %>%
+    select(group) %>%
+    unique() %>%
+    pull())
+  )
 
 
 
