e22368e59c342878274c56aee190dabcc4aa8307
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-02-25 22:20:54 -0500

    change to loading from quarters (easier exporting)

--- load.R
+++ load.R
@@ -1,6 +1,21 @@
 library(tidyverse)
 library(lubridate)
 
-entries <- read_csv("data/source/entries.csv")
-entries <- entries %>%
+entries <- tibble(path = fs::dir_ls("data/source/quarters/", regexp = "\\.csv$")) %>%
+  pull(path) %>%
+  map_dfr(
+    read_csv,
+    col_types = cols(
+      date = col_date(format = ""),
+      client = col_character(),
+      project = col_character(),
+      note = col_character(),
+      billable = col_logical(),
+      billed = col_logical(),
+      rate = col_logical(),
+      start = col_time(format = ""),
+      end = col_time(format = ""),
+      hours = col_double()
+    )
+  ) %>%
   select(date, client, project, note, start, end, hours)
