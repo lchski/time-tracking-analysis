a11bc5f79904f4c9ac95a429cb7de15ee804158d
Lucas Cherkewski <lucas.cherkewski@cds-snc.ca>
2021-03-29 16:19:08 -0400

    refine work block imagery

--- analysis/work-block-type.R
+++ analysis/work-block-type.R
@@ -4,7 +4,7 @@ tagged_work_blocks <- entries_work %>%
   mutate(block_type = str_remove(block_type, fixed("}"))) %>%
   mutate(week = floor_date(date, "1 week", week_start = 1))
 
-tagged_work_blocks %>%
+tagged_work_blocks_summary <- tagged_work_blocks %>%
   mutate(block_type = case_when(
     block_type == "hd" ~ "heads-down",
     block_type == "ms" ~ "meeting (1:1)",
@@ -13,7 +13,20 @@ tagged_work_blocks %>%
   )) %>%
   group_by(week, block_type) %>%
   summarize(hrs = sum(hours)) %>%
+  mutate(hrs_prop = hrs / sum(hrs))
+
+tagged_work_blocks_summary %>%
   ggplot(aes(x = week, y = hrs, fill = block_type)) +
   geom_col(position = "fill") +
   ylab("% of time") +
   labs(title = "Lucas’s weeks")
+
+tagged_work_blocks_summary %>%
+  ggplot(aes(x = week, y = hrs_prop, fill = block_type)) +
+  geom_line() +
+  facet_grid(vars(block_type)) +
+  ylim(c(0,1)) +
+  ylab("% of time") +
+  labs(title = "Lucas’s weeks")
+
+
